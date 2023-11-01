2aa08fa7651ac90f80e6f88194e5e119ba97f4f3
Fotis <fotis.sik.games@gmail.com>
2023-11-01 23:50:00 +0200

    [#7] Added pydantic responses, added user model, added password hasing, code restructure

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from typing import Optional
+from typing import List
 from fastapi import Body, Depends, FastAPI, Response, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, StrictStr
 from random import randrange
+
+from app.utils import hash_password
+
 from . import models, schemas
 import json
 import psycopg2
@@ -11,68 +14,27 @@ from psycopg2.extras import RealDictCursor
 from app.database import SessionLocal, engine, get_db
 from sqlalchemy.orm import Session
 
-from app.database import DatabaseHandler
-
 app = FastAPI()
 
-# create DB tables from the models we defined (if they don't exist)
-# ! it does not let you modify tables, we need migrations (Alembic) for that !
+# Create DB tables from the models we defined (if they don't exist)
+# CAUTION: it does not let you modify tables, we need migrations (Alembic) for that
 models.Base.metadata.create_all(bind=engine)
-
-# Database Connection using psycopg2
-handler = DatabaseHandler()
-conn = handler.get_connection() 
-cursor = conn.cursor()
-print("Database connection was successful")
-
-my_posts = [
-    {   
-        "id": 1,
-        "title": "title of post 1",
-        "content": "content of post 1"
-    },
-    {
-        "id": 2,
-        "title": "favorite foods",
-        "content": "I like pizza"
-    }
-]
-
-def find_post(id: int):
-    """
-    - We create a generator object using () instead of [] 
-    - Every generator is an iterator, not the other way around.
-    - We apply next(iterator_obj, default_value) on it (None if nothing is found).
-    - Every iterator has to have __next__() and __iter__() implemented.
-    - List is an iterable that has __iter()__ implemented. -> that's why iter(list_object) results in an iterator
-    - We use an iterator if we want to traverse a list just once and do nothing more with it.
-    """
-    return next((post for post in my_posts if post["id"] == id), None)
-
-def find_index(id: int):
-    for i, post in enumerate(my_posts):
-        if post["id"] == id:
-            return i
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
 
-# GET all
-@app.get("/posts")
+# GET ALL POSTS
+@app.get("/posts", response_model=List[schemas.PostResponse])
 async def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}  # FASTAPI automatically serializes my_posts into JSON
+    return posts  
+    # FASTAPI automatically serializes posts into JSON
     # return JSONResponse(content=jsonable_encoder(posts))
 
-# GET one
-@app.get("/posts/{id}")
-async def get_post(id: int, db: Session = Depends(get_db)): #, response: Response):   #___________ id: int means that `id` has to be an integer and it is going to try to convert the `id` to an int every time
-    # post = find_post(id)
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
+# GET A POST
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
+async def get_post(id: int, db: Session = Depends(get_db)): #___________ id: int means that `id` has to be an integer and it is going to try to convert the `id` to an int every time
     post = db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -80,19 +42,12 @@ async def get_post(id: int, db: Session = Depends(get_db)): #, response: Respons
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} not found"
         )
-    return {"data": post}
+    return post
 
-# CREATE
-@app.post("/posts", status_code=status.HTTP_201_CREATED) # ________ with status_code we are changing the default response
+# CREATE A POST
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse) # ________ with status_code we are changing the default response
 # async def create_posts(payload: dict = Body(...)):
 async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #     (post.title, post.content, post.published)
-    # )
-    # new_post = cursor.fetchone()
-    # conn.commit()
-
     # create new ost object
     new_post = models.Post(**post.model_dump())
     # add post to DB
@@ -100,39 +55,28 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # commit post to DB
     db.commit()
     # retrieve the post we created and store it back into the variable new_post
-    db.refresh(new_post)    # 
-    return {"data": new_post}                           #_________________________________ send the newly created post back to the Front End
+    db.refresh(new_post)
+    return new_post
 
-# DELETE
+# DELETE A POST
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int, db: Session = Depends(get_db)):
-    # index = find_index(id)
-    # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-
-    # return the sqlalchemy.orm.query.Query object
     post_query = db.query(models.Post).filter(models.Post.id==id)
     # check models.Post object
     if post_query.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found"
         )
-    # conn.commit()
     post_query.delete(synchronize_session=False)
     db.commit()
     return
-    # return Response(status_code=status.HTTP_204_NO_CONTENT) # ______ we deleted the post, we should not send any data back fast api throws an arror if we return the id of the post or something more
+    # return Response(status_code=status.HTTP_204_NO_CONTENT) 
+    # we deleted the post, we should not send any data back fast api throws an error
+    # if we return the id of the post or something more
 
-# UPDATE
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
+# UPDATE A POST
+@app.put("/posts/{id}", status_code=status.HTTP_200_OK, response_model=schemas.PostResponse)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # index = find_index(id)
-    # payload_dict = payload.model_dump()
-    # cursor.execute(
-    #     """UPDATE products SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *""",
-    #     (post.title, post.content, post.published, str(id))
-    # )
-    # updated_post = cursor.fetchone()
     post_query = db.query(models.Post).filter(models.Post.id==id)
     post_to_be_updated = post_query.first()
     if post_to_be_updated is None:
@@ -142,5 +86,31 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
         )
     post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
-    # conn.commit()
-    return {"data": post_to_be_updated} # equals to return JSONResponse(content=jsonable_encoder(payload_dict))
+    return post_to_be_updated # equals to return JSONResponse(content=jsonable_encoder(payload_dict))
+
+# CREATE USER
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password - user.password
+    hashed_password = hash_password(user.password)
+    # replace the plain text password in user
+    user.password = hashed_password
+
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get("/users/{id}", response_model=schemas.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} not found"
+        )
+    return user
